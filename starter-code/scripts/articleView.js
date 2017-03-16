d8424f2e66349a56410468b75e11b6227124c49c
Enrique Rico <enriquesantosrico@gmail.com>
2017-03-16 09:58:45 -0700

    finished the mvp

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -56,17 +56,24 @@ articleView.handleMainNav = function () {
 articleView.setTeasers = function() {
   // Truncate logic to show only first two elements within the article body.
   $('.article-body *:nth-of-type(n+2)').hide();
+  $('article').on('click', '.read-on' ,function(event){
+    event.preventDefault();
+    $(this).parent().find('*').show();
+    $(this).hide();
+  })
   /* TODO: Add a delegated event handler to reveal the remaining paragraphs.
     When a .read-on link is clicked, we can:
     1. Prevent the default action of a link.
-    2. Reveal everythi    ng in that particular article now.
+    2. Reveal everything in that particular article now.
     3. Hide that read-on link!
 
     // STRETCH GOAl!: change the 'Read On' link to 'Show Less'
   */
 };
+articleView.setTeasers();
 articleView.populateFilters();
 articleView.handleAuthorFilter();
 articleView.handleCategoryFilter();
 articleView.handleMainNav();
+// articleView.setTeasers();
 // TODO: Invoke all of the above functions (I mean, methods!):
